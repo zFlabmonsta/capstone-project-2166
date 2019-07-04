e56fecb80397be42adefb5ac045930727a7e401c
thinkpad <peeter._@hotmail.com>
2019-07-04 22:15:22 +1000

    note

--- src/main/urls.py
+++ src/main/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views     # from current path import views.py
 
 urlpatterns=[
-    path("", views.index, name="index"),        
+    path("no", views.index, name="index"),        
+    path("about", views.about, name="about"),
+    path("howto", views.howto, name="howto"),
+    path("dashboard", views.dashboard, name="dashboard"),
 ]
--- src/main/views.py
+++ src/main/views.py
@@ -2,5 +2,16 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+
+# Home Page
 def index(response):
-    return HttpResponse("<h1>fuck you!</h1>")
+    return HttpResponse("<h1>Home Page!</h1>")
+
+def about(response):
+    return HttpResponse("<h1> About! </h1>")
+
+def dashboard(response):
+    return HttpResponse("<h1> Dashboard! </h1>")
+
+def howto(response):
+    return HttpResponse("<h1> Howto !</h1>")
