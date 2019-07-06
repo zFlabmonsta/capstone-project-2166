4c2fd74a27b5ad8b112b52fc1dcbad6879beaf42
thinkpad <peeter._@hotmail.com>
2019-07-06 11:23:20 +1000

    created templates for dynamic page

--- src/main/models.py
+++ src/main/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class Dashboard(models.Model):
+    """
+    Each user that signs up will be assigned their on Dashboard
+    to hold and save their classes, where each class will 
+    save the lecturer's evaluated learning outcome 
+    """
+    dashboard_name = models.CharField(max_length=200)
+
+class MyClass(models.Model):
+    """
+    MyClass is created by the lecturer and stores learning outcomes
+    that are evaluated and wanted.
+    """
+    dashboard = models.ForeignKey(Dashboard, on_delete=models.CASCADE)
+    class_name = models.CharField(max_length=20)
+
+class Learning_outcome(models.Model):
+    """
+    contains learning outcomes
+    """
+    my_class = models.ForeignKey(MyClass, on_delete=models.CASCADE)
+    learning_outcome = models.CharField(max_length=1000)
+
--- src/main/views.py
+++ src/main/views.py
@@ -5,13 +5,19 @@ from django.http import HttpResponse
 
 # Home Page
 def index(response):
-    return HttpResponse("<h1>Home Page!</h1>")
+    return render(response, "main/index.html", {})
 
 def about(response):
     return HttpResponse("<h1> About! </h1>")
 
+"""
+TODO:
+    Display Dashboard's Classes [ ]
+    Link Dashboard to Registered User [ ]
+    Redirect add class button to create class to dashboard [ ]
+"""
 def dashboard(response):
-    return HttpResponse("<h1> Dashboard! </h1>")
+    return render(response, "main/dashboard.html", {})
 
 def howto(response):
     return HttpResponse("<h1> Howto !</h1>")
