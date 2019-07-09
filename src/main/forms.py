a19c3515f594658bc8ff52db613036c79e176db6
thinkpad <peeter._@hotmail.com>
2019-07-09 16:42:25 +1000

    pivot to accommodation

--- src/main/forms.py
+++ src/main/forms.py
@@ -2,6 +2,6 @@ from django import forms
 
 class Property_form (forms.Form):
     street = forms.CharField(label='street', max_length=50)
-    post_code = forms.CharField(label='post_code', max_length=20)
+    post_code = forms.CharField(label='post code', max_length=20)
     suburb = forms.CharField(label='suburb', max_length=50)
     # images = 
